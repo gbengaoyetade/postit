8416d207cb1ff6bacabc33159a2717f21b28837a
gbenga_ps <ioyetade@gmail.com>
2017-07-31 18:05:38 +0100

    modify config

--- server/models/index.js
+++ server/models/index.js
@@ -3,13 +3,15 @@ import path from 'path';
 import Sequelize from 'sequelize';
 import configs from '../config/config.json';
 
+require('dotenv').config();
+
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = configs[env];
-
 const db = {};
 
 let sequelize;
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
