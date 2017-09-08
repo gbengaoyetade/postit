460bd46122fcfed4fc06e9220870b4959e64a7d6
Andela Developer <andeladeveloper@gbenga-ps.local>
2017-09-08 19:02:51 +0100

    modify db config

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ const config = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'DATABASE_PRODUCTION_URL',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
   },
   test: {
