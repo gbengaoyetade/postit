e11767a9a92e45759a6e0cf1f4f5f104b9b318b2
gbengaPS <ioyetade@gmail.com>
2017-09-12 15:54:30 +0100

    add getMessages and getGroupMembers endpoint

--- server/route.js
+++ server/route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { signUp, signIn, resetPassword, updatePassword } from './controllers/userController';
-import { create, addMembers, getGroups, leaveGroup } from './controllers/groupController';
+import { create, addMembers, getGroups, leaveGroup, getGroupMembers } from './controllers/groupController';
 import { createMessage, getMessages } from './controllers/messageController';
 import authenticate from './middleware/authenticate';
 import { groupAndUserExist, groupExist } from './middleware/exist';
@@ -23,9 +23,11 @@ router.post('/group/:groupId/user', groupAndUserExist, addMembers);
 
 router.post('/group/:groupId/message', createMessage);
 
-router.get('/group/:groupId/messages', groupAndUserExist, getMessages);
+router.get('/group/:groupId/messages', groupExist, getMessages);
 
-router.get('/group', getGroups);
+router.get('/group/user', getGroups);
+
+router.get('/group/:groupId/users', groupExist, getGroupMembers);
 
 router.delete('/group/:groupId/user', groupExist, leaveGroup);
 
