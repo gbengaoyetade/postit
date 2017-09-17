b95e5bad8227d10cc400862e24c3f0a3702222bd
gbengaPS <ioyetade@gmail.com>
2017-09-17 18:27:23 +0100

    Remove unused migration files

--- server/migrations/20170915191706-add-readBy-to-messages.js
+++ server/migrations/20170915191706-add-readBy-to-messages.js
@@ -1,11 +1,11 @@
-module.exports = {
-  up(queryInterface, Sequelize) {
-    return queryInterface.addColumn('messages', 'readBy', {
-      type: Sequelize.INTEGER,
-    });
-  },
+// module.exports = {
+//   up(queryInterface, Sequelize) {
+//     return queryInterface.addColumn('messages', 'readBy', {
+//       type: Sequelize.INTEGER,
+//     });
+//   },
 
-  down(queryInterface) {
-    return queryInterface.removeColumn('messages', 'readBy');
-  },
-};
+//   down(queryInterface) {
+//     return queryInterface.removeColumn('messages', 'readBy');
+//   },
+// };
